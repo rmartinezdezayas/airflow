44877686e7c62a452b1564f25b334ccad5792be8
DSS <dss@home.com>
2022-10-06 23:36:43 +0200

    Created script to set up airflow.

--- 01_set_up_airflow.sh
+++ 01_set_up_airflow.sh
@@ -1,2 +1,20 @@
-#code to install docker and set up airflow
-nohup sudo docker-compose up &
+sudo apt-get update
+sudo apt-get install \
+	ca-certificates \
+	curl \
+	gnupg \
+	lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo service docker start
+sudo docker run hello-world
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo chmod 777 dags logs scripts
+nohup sudo docker compose up >/dev/null 2>&1 &
