87e14742db41faf90643edc0eea681158b886bde
DSS <dss@home.com>
2022-10-06 20:04:58 +0200

    Automated getting the airflow container id to create airflow user once installed and up.

--- 02_create_airflow_user.sh
+++ 02_create_airflow_user.sh
@@ -1,5 +1,6 @@
 #get docker id from docker ps
-sudo docker exec -it 2703bb00db11 airflow users create \
+AIRFLOW_WEBSERVER_CONTAINER=`sudo docker ps | grep webserver | awk '{print $1}'`
+sudo docker exec -it $AIRFLOW_WEBSERVER_CONTAINER airflow users create \
 	--username admin \
 	--password admin \
 	--firstname Peter \
